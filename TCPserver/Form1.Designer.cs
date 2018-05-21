0e3641e700d79f4af93e372e1df0b6c30ad74332
William <bcon615@gmail.com>
2018-05-21 10:37:28 -0400

    Add files via upload

---
+++ TCPserver/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TCPserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(655, 203);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(626, 530);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(158, 23);
+            this.btnStartServer.TabIndex = 1;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.BtnStartServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 582);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStartServer;
+    }
+}
+
---
+++ TCPserver/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+
+namespace TCPserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStartServer_Click(object sender, EventArgs e)
+        {
+            Thread tcpServerRunThread = new Thread(new ThreadStart(TcpServerRun));
+            tcpServerRunThread.Start();
+        }
+
+        private void TcpServerRun()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 5004);
+            listener.Start();
+            UpdateUI("Listening");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                UpdateUI("Connected");
+
+                using (var process = new Process())
+                {                 
+                    process.StartInfo.FileName = "C:\\Program Files\\"; // add full path
+                    process.Start();
+                    UpdateUI("Open Application");
+                    process.WaitForExit();
+                }
+
+                Thread tcpHandlerThread = new Thread(new ParameterizedThreadStart(TcpHandler));
+                tcpHandlerThread.Start(client);
+
+            }
+        }
+
+        private void TcpHandler(object client)
+        {
+            TcpClient mClient = (TcpClient)client;
+            NetworkStream stream = mClient.GetStream();
+            byte[] message = new byte[1024];
+            stream.Read(message, 0, message.Length);
+            UpdateUI("New Message = " + Encoding.ASCII.GetString(message));
+
+            stream.Close();
+            mClient.Close();
+
+        }
+
+        private void UpdateUI(string s)
+        {
+            Func<int> del = delegate ()
+            {
+                textBox1.AppendText(s + System.Environment.NewLine);
+                return 0;
+            };
+            Invoke(del);
+        }
+    }
+}
